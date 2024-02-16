c5c2d4f31b4c23ec997572ade2b28c771c35d698
Samuel Hoile <samuelhoile@Samuels-MacBook-Pro.local>
2024-02-16 14:08:26 +0000

    initials actions

--- backend/dbServer.js
+++ backend/dbServer.js
@@ -1,4 +1,4 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize } = require('sequelize');
 const express = require("express")
 const cors = require('cors')
 const app = express()
